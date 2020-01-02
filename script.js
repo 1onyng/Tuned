76fa57b06e7d4c04389642220f91ad8e6dd77bbf
tonyng <tonyng987@gmail.com>
2020-01-02 08:40:12 -0800

    Add bar graph and star animation

--- script.js
+++ script.js
@@ -1,203 +1,178 @@
 window.onload = function () {
-  var audio,
+  let audio,
     analyser,
-    audioContext,
-    soundSource,
-    soundBuffer,
-    sourceNode,
-    cycle,
-    transparency,
+    audioCtx,
+    canvas,
+    gradient,
+    source,
+    bufferLength,
     freqArray,
     stream;
 
-  var hue = hue || 280;
-  var display = "circles";
-  var audioInput = document.getElementById('audiofile');
-  var myAudio = document.querySelector('audio');
+  let display = "circles";
+  let audioInput = document.getElementById('audiofile');
+  let sample = document.getElementsByClassName('sample')[0];
 
   audioInput.addEventListener('change', function (event) {
-    if (!audioContext || audioContext.state !== "running") {
+    if (!audioCtx || audioCtx.state !== "running") {
       stream = URL.createObjectURL(event.target.files[0]);
       audio = new Audio(stream);
-      setup();
+      setupAudio();
     }
   });
-
-
-  var sample = document.getElementsByClassName('sample')[0];
 
   sample.addEventListener('click', function () {
     // audio = new Audio('https://s3.us-east-2.amazonaws.com/sparkify2019/whatsgoing/1.mp3');
     audio = new Audio('gagging_order.mp3');
     // audio.crossOrigin = "anonymous";
-    setup();
+    setupAudio();
   });
 
-  function startSound() {
-    debugger;
-    sourceNode = audioContext.createMediaElementSource(audio);
-    sourceNode.connect(analyser);
-    sourceNode.connect(audioContext.destination);
-    audio.play();
-    debugger;
-    update();
-  }
-
-  function setup() {
+  function setupAudio() {
     audio.addEventListener('canplay', function () {
-      audioContext = audioContext || new AudioContext();
-      analyser = (analyser || audioContext.createAnalyser());
+      audioCtx = new AudioContext();
+      analyser = audioCtx.createAnalyser();
       analyser.smoothingTimeConstant = 0.1;
       analyser.fftSize = 512;
-
-      startSound();
+      source = audioCtx.createMediaElementSource(audio);
+      source.connect(analyser);
+      analyser.connect(audioCtx.destination);
+      audio.play();
+      animate();
     });
   }
-  document.getElementsByClassName('play')[0].addEventListener('click', playSound.bind(null, sourceNode));
+  document.getElementsByClassName('play')[0].addEventListener('click', playSound.bind(null, source));
   document.getElementsByClassName('stop')[0].addEventListener('click', stopSound);
 
-
   function playSound() {
-    if (audioContext) {
-      if (audioContext.state === 'suspended') {
-        audioContext.resume();
+    if (audioCtx) {
+      if (audioCtx.state === 'suspended') {
+        audioCtx.resume();
       }
     }
   }
 
   function stopSound() {
-    if (audioContext) {
-      if (audioContext.state === 'running') {
-        audioContext.suspend();
+    if (audioCtx) {
+      if (audioCtx.state === 'running') {
+        audioCtx.suspend();
       }
     }
   }
 
+  function drawCircle(ctx, freqArray, analyser, width, height) {
+    let centerX = width / 2,
+      centerY = height / 2,
+      radius = 150,
+      bars = 170,
+      barWidth = 2,
+      barHeight;
 
-  function drawCircle(context, freqValue, freqSequence) {
-    var x = ((width / 2)),
-      y = ((height / 2));
-    if (false) {
-      transparency = 0.001;
-    } else {
-      transparency = 0.08;
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    analyser.getByteFrequencyData(freqArray);
+    for (let i = 0; i < bars; i++) {
+      let rads = Math.PI * 2 / bars;
+      let lineColor = "rgb(" + freqArray[i] + ", " + freqArray[i] + ", " + 205 + ")";
+      barHeight = freqArray[i] * 0.7;
+
+      x = centerX + Math.cos(rads * i) * (radius);
+      y = centerY + Math.sin(rads * i) * (radius);
+      xEnd = centerX + Math.cos(rads * i) * (radius + barHeight);
+      yEnd = centerY + Math.sin(rads * i) * (radius + barHeight);
+
+      ctx.strokeStyle = lineColor;
+      ctx.lineWidth = barWidth;
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(xEnd, yEnd);
+      ctx.stroke();
     }
-    context.beginPath();
-    context.arc(x - (freqValue / 8), y - (freqValue / 8), (Math.abs(freqValue - 120)) * 5, 0, 2 * Math.PI, false);
-    context.strokeStyle = 'hsla(' + hue + ', ' + 100 + '%,' + 40 + '%,' + transparency + ')';
-    context.fillStyle = "hsla(" + hue + ", 100%, 40%, .01)";
-
-    context.fill();
-    context.lineWidth = 2;
-    context.stroke();
   }
 
-  function drawOscillo(context, freqArray) {
+  function drawBarGraph(ctx, freqArray, bufferLength, analyser, width, height) {
+    let barWidth = (width / bufferLength) * 2.5;
+    let barHeight;
+    let x = 0;
 
-    context.beginPath();
-    var sliceWidth = canvas.width * 1.0 / freqArray.length;
+    analyser.getByteFrequencyData(freqArray);
 
-    var oscilloX = 0;
-    for (var i = 0; i < freqArray.length; i++) {
+    ctx.fillRect(0, 0, width, height);
 
-      var v = freqArray[i] / 86.0;
-      var y = v * 200 / 2;
+    for (let i = 0; i < bufferLength; i++) {
+      barHeight = freqArray[i] * 1.5;
 
-      if (i === 0) {
-        context.moveTo(oscilloX, y);
-      } else {
-        context.lineTo(oscilloX, y);
-      }
+      let r = barHeight + (25 * (i / bufferLength));
+      let g = 250 * (i / bufferLength);
+      let b = 50;
 
-      oscilloX += sliceWidth;
+      ctx.fillStyle = "rgb(" + freqArray[i] + ", " + freqArray[i] + ", " + 205 + ")";
+      ctx.fillRect(x, height - barHeight, barWidth, barHeight);
+
+      x += barWidth + 1;
     }
-
-    if (false) {
-      transparency = 0.001;
-    } else {
-      transparency = 0.08;
-    }
-    context.strokeStyle = 'hsla(' + hue + ', ' + 100 + '%,' + 40 + '%,' + 0.9 + ')';
-    context.lineTo(canvas.width, canvas.height / 2);
-    context.lineWidth = 2;
-    context.stroke();
   }
 
+  canvas = document.getElementById('canvas'),
+  width = canvas.width = window.innerWidth,
+  height = canvas.height = window.innerHeight,
+  ctx = canvas.getContext('2d');
 
-  var canvas = document.getElementById('canvas'),
-    context = canvas.getContext('2d'),
-    width = canvas.width = window.innerWidth,
-    height = canvas.height = window.innerHeight,
-    c = 0;
+  gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+  gradient.addColorStop(0, "rgba(35, 7, 77, 1)");
+  gradient.addColorStop(1, "rgba(0, 100, 0, 1)");
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  function update() {
-    context.clearRect(0, 0, width, height);
-    freqArray = new Uint8Array(analyser.frequencyBinCount);
-    analyser.getByteTimeDomainData(freqArray);
-    oscilloX = 0;
+  function animate() {
+    ctx.clearRect(0, 0, width, height);
+    
+    gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+    gradient.addColorStop(0, "rgba(35, 7, 77, 1)");
+    gradient.addColorStop(1, "rgba(0, 100, 0, 1)");
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    bufferLength = analyser.frequencyBinCount;
+    freqArray = new Uint8Array(bufferLength);
+
     if (display === "circles") {
-
-      for (var i = 0; i < freqArray.length; i += 1) {
-        var point = freqArray[i];
-        drawCircle(context, point, i);
-
-      }
-    } else if (display === "oscillo") {
-      drawOscillo(context, freqArray);
+      drawCircle(ctx, freqArray, analyser, width, height);
+    } else if (display === "barGraph") {
+      drawBarGraph(ctx, freqArray, bufferLength, analyser, width, height);
     }
 
-    if (cycle && audioContext.state === 'running') {
-      hue += 1;
-    }
-    requestAnimationFrame(update);
+    requestAnimationFrame(animate);
   }
 
-  var options = document.getElementsByClassName('options-header')[0];
+  let options = document.getElementsByClassName('options-header')[0];
 
   options.addEventListener("click", function () {
-    var accordion = document.getElementsByClassName('options-list')[0];
+    let accordion = document.getElementsByClassName('options-list')[0];
     accordion.classList.toggle("hiding");
   })
 
-  var colorpicker = document.getElementsByClassName('rainbow')[0];
-  colorpicker.addEventListener("click", function (event) {
-    var rect = colorpicker.getBoundingClientRect();
-    var percent = (event.pageX - rect.left) / colorpicker.offsetWidth;
-    hue = 360 * percent;
-    console.log(event.pageX - rect.left);
-  })
+  let barGraphButton = document.getElementById('barGraph');
+  let circlesButton = document.getElementById('circles');
 
-  var cycleButton = document.getElementsByClassName('cycle')[0];
-
-  cycleButton.addEventListener("click", function () {
-    if (cycle) {
-      cycle = false;
-    } else {
-      cycle = true;
-    }
-  })
-
-  var oscilloButton = document.getElementById('oscillo');
-  var circlesButton = document.getElementById('circles');
-
-  oscilloButton.addEventListener("click", function () {
-    display = "oscillo";
-    console.log(display);
+  barGraphButton.addEventListener("click", function () {
+    display = "barGraph";
   });
 
   circlesButton.addEventListener("click", function () {
     display = "circles";
-    console.log(display);
   })
-  var question = document.getElementsByClassName('help')[0];
-  var closeButton = document.getElementsByClassName('close')[0];
-  var modal = document.getElementsByClassName('modal')[0];
+  // let question = document.getElementsByClassName('help')[0];
+  // let closeButton = document.getElementsByClassName('close')[0];
+  // let modal = document.getElementsByClassName('modal')[0];
 
-  closeButton.addEventListener("click", function () {
-    modal.classList.add("closed");
-  })
+  // closeButton.addEventListener("click", function () {
+  //   modal.classList.add("closed");
+  // })
 
-  question.addEventListener("click", function () {
-    modal.classList.remove("closed");
-  })
+  // question.addEventListener("click", function () {
+  //   modal.classList.remove("closed");
+  // })
 };
